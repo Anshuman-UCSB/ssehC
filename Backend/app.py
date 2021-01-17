78b9228a3c00634be1b39a85d7165f8d7a9e8496
Anshuman-UCSB <anshuman@ucsb.edu>
2021-01-17 01:24:16 -0800

    safe certicifactes trust me

--- Backend/app.py
+++ Backend/app.py
@@ -116,10 +116,6 @@ def makeID():
 
 api.add_resource(apiHandler, '/')
 
-from OpenSSL import SSL
-context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
-context.use_privatekey_file('server.key')
-context.use_certificate_file('server.crt')   
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0",ssl_context=context)
+    app.run(host = "0.0.0.0",ssl_context='adhoc')
